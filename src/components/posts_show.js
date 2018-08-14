b91c7c479b1f0d11c9b1c7108af23422a67301a8
Adam LaCasse <adamlacasse@outlook.com>
2018-08-14 15:14:03 -0400

    form is rendering but it's not working

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
 import {Link} from 'react-router-dom';
-import {fetchPost, deletePost} from '../actions';
+import {fetchPost, deletePost, updatePost} from '../actions';
 
 class PostsShow extends Component {
     componentDidMount() {
@@ -16,8 +17,23 @@ class PostsShow extends Component {
         });
     }
 
+    renderField(field){
+        const { meta: { touched, error } } = field;
+        // const className = `form-group ${touched && error ? 'has-danger' : "" }`
+        return (
+            <div className='form-group'>
+                <label>{field.label}</label>
+                <input
+                    className="form-control"
+                    type="text"
+                    {...field.input}
+                />
+            </div>
+        );
+    }
+
     render() {
-        const { post } = this.props;
+        const { post, handleSubmit } = this.props;
 
         if (!post) {
             return <div>Loading...</div>
@@ -32,9 +48,29 @@ class PostsShow extends Component {
                 >
                     Delete Post
                 </button>
-                <h3>{post.title}</h3>
-                <h6>Categories: {post.categories}</h6>
-                <p>{post.content}</p>
+                <form //onSubmit={handleSubmit(this.onSubmit.bind(this))}
+                >
+                    <Field
+                        label="Title"
+                        name="title"
+                        placeholder={post.title} 
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Categories"
+                        name="categories"
+                        placeholder={post.categories} 
+                        component={this.renderField}
+                    />
+                    <Field
+                        label="Post Content"
+                        name="content"
+                        placeholder={post.content} 
+                        component={this.renderField}
+                    />
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                    <Link to="/" className="btn btn-danger">Cancel</Link>
+                </form>
             </div>
         );
     }
@@ -44,4 +80,8 @@ function mapStateToProps({ posts }, ownProps) {
     return { post: posts[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
+export default reduxForm({
+    form: 'EditPostForm'
+})(
+    connect(mapStateToProps, {fetchPost, deletePost, updatePost})(PostsShow)
+);
