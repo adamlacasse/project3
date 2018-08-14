e4df0d114d616074e082d72abda3e39225b489f5
Adam LaCasse <adamlacasse@outlook.com>
2018-08-14 14:32:18 -0400

    added updatePost to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
 export const CREATE_POST = 'create_post';
-export const DELETE_POST = 'delete_post'
+export const DELETE_POST = 'delete_post';
+export const UPDATE_POST = 'update_post';
 
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api'
@@ -44,5 +45,15 @@ export function deletePost(id, callback) {
     return {
         type: DELETE_POST,
         payload: id
+    }
+}
+
+export function updatePost(id, callback) {
+    const request = axios.put(`${ROOT_URL}/posts/${id}${API_KEY}`)
+        .then(() => callback());
+
+    return {
+        type: UPDATE_POST,
+        payload: request
     }
 }
